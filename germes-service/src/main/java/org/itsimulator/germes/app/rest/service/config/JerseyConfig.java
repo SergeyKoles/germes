acc481ede1cb94b7d20fd4ef2434dc20e1e17baa
Сергей Колесников <skolesnikov@systematica-consulting.ru>
2020-07-31 07:46:48 +0300

    sources-9-51 - add JerseyTest

---
+++ germes-service/src/main/java/org/itsimulator/germes/app/rest/service/config/JerseyConfig.java
@@ -0,0 +1,18 @@
+package org.itsimulator.germes.app.rest.service.config;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("api")
+/**
+ * REST web-service configuration for Jersey
+ * @author Morenets
+ *
+ */
+public class JerseyConfig extends ResourceConfig {
+  public JerseyConfig() {
+    packages("org.itsimulator.germes.app.rest");
+  }
+}
+
