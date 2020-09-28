b9a53c8af04cd2213a0e3f0409c1a0ee1c46fb25
Сергей Колесников <skolesnikov@systematica-consulting.ru>
2020-09-28 07:12:20 +0300

    sources-30-87

--- germes-service/src/main/java/org/itsimulator/germes/app/service/impl/GeographicServiceImpl.java
+++ germes-service/src/main/java/org/itsimulator/germes/app/service/impl/GeographicServiceImpl.java
@@ -1,13 +1,17 @@
 package org.itsimulator.germes.app.service.impl;
 
-import org.itsimulator.germes.app.infra.util.CommonUtil;
 import org.itsimulator.germes.app.model.entity.geography.City;
 import org.itsimulator.germes.app.model.entity.geography.Station;
 import org.itsimulator.germes.app.model.search.criteria.StationCriteria;
 import org.itsimulator.germes.app.model.search.criteria.range.RangeCriteria;
+import org.itsimulator.germes.app.persistence.repository.CityRepository;
 import org.itsimulator.germes.app.service.GeographicService;
 
-import java.util.*;
+import javax.inject.Inject;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -16,52 +20,34 @@ import java.util.stream.Collectors;
  * @author Morenets
  */
 public class GeographicServiceImpl implements GeographicService {
-  /**
-   * Internal list of cities
-   */
-  private final List<City> cities;
+  private final CityRepository cityRepository;
 
-  /**
-   * Auto-increment counter for entity id generation
-   */
-  private int counter = 0;
-
-  private int stationCounter = 0;
-
-  public GeographicServiceImpl() {
-    cities = new ArrayList<City>();
+  @Inject
+  public GeographicServiceImpl(CityRepository cityRepository) {
+    this.cityRepository = cityRepository;
   }
 
   @Override
   public List<City> findCities() {
-    return CommonUtil.getSafeList(cities);
+    return cityRepository.findAll();
   }
 
   @Override
   public void saveCity(City city) {
-    if (!cities.contains(city)) {
-      city.setId(++counter);
-      cities.add(city);
-    }
-    city.getStations().forEach((station) -> {
-      if (station.getId() == 0) {
-        station.setId(++stationCounter);
-      }
-    });
+    cityRepository.save(city);
   }
 
   @Override
   public Optional<City> findCitiyById(final int id) {
-    return cities.stream().filter((city) -> city.getId() == id).findFirst();
+    return Optional.ofNullable(cityRepository.findById(id));
   }
 
   @Override
   public List<Station> searchStations(final StationCriteria criteria, final RangeCriteria rangeCriteria) {
     Set<Station> stations = new HashSet<>();
-    for (City city : cities) {
-      stations.addAll(city.getStations());
-    }
 
-    return stations.stream().filter((station) -> station.match(criteria)).collect(Collectors.toList());
+    cityRepository.findAll().forEach(city -> stations.addAll(city.getStations()));
+
+    return stations.stream().filter(station -> station.match(criteria)).collect(Collectors.toList());
   }
 }
